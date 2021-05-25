751ea5d421529b2da922f264ca70319ec6332503
lindomarc <lindomarc@outlook.com>
2021-05-25 00:22:46 -0300

    fix

--- src/DB/Sql.php
+++ src/DB/Sql.php
@@ -1,50 +1,43 @@
 <?php 
 
-namespace PdoConnect\DB;
+namespace Lindomar\PhpClass\DB;
+
 
 class Sql {
 	
 	private $conn;
+	
 	private $config;
 	
 	public function __construct()
 	{
 		$this->loadConfig();
 		
- 		$this->conn = new \PDO('mysql:host='.$this->config->HOSTNAME.';dbname='.$this->config->DBNAME, 
-			$this->config->USERNAME,
-			$this->config->PASSWORD
-		);
-
+		$dsn = "mysql:dbname={$this->config->DBNAME};host={$this->config->HOSTNAME}";
+		
+		$username = $this->config->USERNAME;
+		$password = $this->config->PASSWORD;
+		
+  		$this->conn = new \PDO($dsn, $username, $password);
 	}
 
 	private function setParams($statement, $parameters = array())
 	{
-
-		foreach ($parameters as $key => $value) {
-			
+		foreach ($parameters as $key => $value) {			
 			$this->bindParam($statement, $key, $value);
-
 		}
-
 	}
 
 	private function bindParam($statement, $key, $value)
 	{
-
 		$statement->bindParam($key, $value);
-
 	}
 
 	public function query($rawQuery, $params = array())
 	{
-
 		$stmt = $this->conn->prepare($rawQuery);
-
 		$this->setParams($stmt, $params);
-
 		$stmt->execute();
-
 	}
 
 	public function select($rawQuery, $params = array()):array
@@ -64,16 +57,22 @@ class Sql {
 	private function loadConfig()
 	{
 		$vendorDir = dirname(dirname(__FILE__));
+		
 		$baseDir = dirname(dirname(dirname($vendorDir)));
+		
 		$file = $baseDir.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config_database.json';
-
+		
 		if(file_exists($file)) {
-			$this->config = json_decode(file_put_contents($file));
-		} else {
-			echo "<p>Not Found config file:</p>";
-			echo '<p>/config/config_database.json</p>';
+			
+			$this->config = json_decode(file_get_contents($file));
+			
+		} else {            			
+			
+			echo "<p>Not Found config file:</p>";			
+			echo '<p>/config/config_database.json</p>';			
 			die();
+			
 		}
 	}
-
+	
 }
